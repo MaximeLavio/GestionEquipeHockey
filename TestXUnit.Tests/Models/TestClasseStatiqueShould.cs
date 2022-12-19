5cbb68cf531f157d8b2215d4ac8ab36f5c3a8328
Maxime Laviolette <2164219@cegepoutaouais.qc.ca>
2022-12-18 22:27:10 -0500

    Ajout test Xunit

---
+++ TestXUnit.Tests/Models/TestClasseStatiqueShould.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestXUnit.Tests.Models
+{
+    public class TestClasseStatiqueShould
+    {
+        [Theory]
+        [InlineData(2022)]
+        public static int Age(int p_date_naissance)
+        {
+            return DateTime.Now.Year - p_date_naissance;
+        }
+
+
+        [Theory]
+        [InlineData(2023, 2018)]
+        public static int Dure(int p_date_debut, int p_date_fin)
+        {
+            return p_date_fin - p_date_debut;
+        }
+    }
+}
